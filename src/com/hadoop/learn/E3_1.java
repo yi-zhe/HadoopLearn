5fa7e528669c6e1250585280498207dab2d94bfb
Yizhe <906436776@qq.com>
2018-06-23 20:52:33 +0800

    E3_1

---
+++ src/com/hadoop/learn/E3_1.java
@@ -0,0 +1,31 @@
+package com.hadoop.learn;
+
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class E3_1 {
+
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) {
+        InputStream in = null;
+        try {
+            in = new URL("hdfs://192.168.0.210/word.in").openStream();
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
