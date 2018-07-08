68832125990eb2d07e2c7b829edb0a9661121b1d
Yizhe <906436776@qq.com>
2018-07-08 18:14:48 +0800

    调试错误的Record信息

--- src/com/hadoop/learn/com/hadoop/learn/ch6/E6_8MaxTemperatureMapper.java
+++ src/com/hadoop/learn/com/hadoop/learn/ch6/E6_8MaxTemperatureMapper.java
@@ -14,10 +14,20 @@ public class E6_8MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, I
 
     private E6_7NcdcRecordParser parser = new E6_7NcdcRecordParser();
 
+    enum Temperature {
+        OVER_100
+    }
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         parser.parse(value);
         if (parser.isValidTemperature()) {
+            int airTemperature = parser.getAirTemperature();
+            if (airTemperature > 1000) {
+                System.err.println("Temperature over 100 degrees for input " + value);
+                context.setStatus("Detected possibly corrupt record: see logs.");
+                context.getCounter(Temperature.OVER_100).increment(1);
+            }
             context.write(new Text(parser.getYear()), new IntWritable(parser.getAirTemperature()));
         }
     }
